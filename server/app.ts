a2165ef607b7a4d6886a020dd915544e3c35ac9d
Andy Tran <undahvt@gmail.com>
2024-11-25 00:09:39 -0800

    testing api, parsers and unknown route for protection

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,33 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 export const app = express();
+
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+require('dotenv').config();
+
+// body parser (using cloud energy)
+app.use(express.json({limit: "50mb"}));
+
+// cookie parser (for sending from frontend to backsend)
+app.use(cookieParser());
+
+// cors => cross origin resource sharing 
+app.use(cors({
+    origin: process.env.ORIGIN
+}));
+
+// testing api 
+app.get("/test", (req:Request , res:Response , next:NextFunction) => {
+    res.status(200).json({
+        success: true,
+        message: "API is working",
+    });
+});
+
+// unknown route
+app.all("*", (req: Request, res:Response, next:NextFunction) => {
+    const err = new Error(`Route ${req.originalUrl} not found`) as any;
+    err.statusCode = 404;
+    next(err);
+});
