0898a42058548a41268a5fd1fb5823a63bd1478f
Andy Tran <undahvt@gmail.com>
2024-11-25 14:09:18 -0800

    comparing hashed password to user input

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -27,6 +27,54 @@ const userSchema: Schema<IUser> = new mongoose.Schema ({
     email: {
         type:String,
         required: [true, "Please enter your email"],
+        validate: {
+            validator: function (value:string){
+                return emailRegexPattern.test(value);
+            },
+            message:"please enter a valid email",
+        },
+        unique: true,
+    },
+    password: {
+        type:String,
+        required: [true, "Please enter your desired password"],
+        minlength: [6, "Password must be at least 6 characters"],
+        select: false,
+    },
+    avatar:{
+        public_id: String,
+        url: String,
+    },
+    role: {
+        type:String,
+        default: "user",
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    courses:[
+        {
+            courseId: String,
+            
+        }
+    ],
 
+},{timestamps: true});
+
+// Hash Password before saving
+userSchema.pre<IUser>('save', async function(next){
+    if(!this.isModified('password')){
+        next();
     }
-})
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+// compare password 
+userSchema.methods.comparePassword = async function(enteredPassword:string): Promise<boolean>{
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const userModel: Model<IUser> = mongoose.model("User", userSchema);
+export default userModel;
