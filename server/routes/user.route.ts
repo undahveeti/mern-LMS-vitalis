302ab77a60aa2e5392dba64566387ef20d80335b
Andy Tran <undahvt@gmail.com>
2024-11-26 15:24:58 -0800

    fixed minor error with put request

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -19,6 +19,6 @@ userRouter.get('/me', isAuthenticated, getUserInfo);
 
 userRouter.post('/social-auth', socialAuth);
 
-userRouter.post('/update-user-info', isAuthenticated, updateUserInfo);
+userRouter.put('/update-user-info', isAuthenticated, updateUserInfo);
 
 export default userRouter;
