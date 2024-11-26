5aef24942405fb9a2eadaee2ddffaff1ccdddf8a
Andy Tran <undahvt@gmail.com>
2024-11-25 22:13:51 -0800

    send token via user controller & route for user login

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -9,6 +9,7 @@ import jwt, {Secret} from "jsonwebtoken";
 import ejs from "ejs";
 import path from "path";
 import sendMail from "../utils/sendMail";
+import { sendToken } from "../utils/jwt";
 
 require("dotenv").config();
 
@@ -138,20 +139,27 @@ interface ILoginRequest {
     password: string;
 }
 
-export const LoginUser = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+export const loginUser = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
     try {
         const {email,password} = req.body as ILoginRequest;
 
         if(!email || !password){
-            return next(new ErrorHandler("Please enter email and password", 400))
+            return next(new ErrorHandler("Please enter email and password", 400));
         }
 
         const user = await userModel.findOne({email}).select("+password");
 
         if(!user){
-            return next(new ErrorHandler("Invalid email or password", 400))
+            return next(new ErrorHandler("Invalid email or password", 400));
         }
         const isPasswordMatch = await user.comparePassword(password);
+
+        if(!isPasswordMatch){
+            return next(new ErrorHandler("Invalid email or password", 400));
+        }
+
+        sendToken(user, 200, res);
+
     } catch (error:any) {
         return next(new ErrorHandler(error.message, 400));
     }
