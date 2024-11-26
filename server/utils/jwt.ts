5d9042d33e75acf5542572d9fd6e1b473e21d87a
Andy Tran <undahvt@gmail.com>
2024-11-26 14:10:22 -0800

    fixed math on expirations

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -26,15 +26,15 @@ export const sendToken = (user:IUser, statusCode: number, res:Response) => {
 
     // options for cookies
     const accessTokenOptions: ITokenOptions = {
-        expires: new Date(Date.now() + accessTokenExpire * 1000),
-        maxAge: accessTokenExpire * 1000,
+        expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
+        maxAge: accessTokenExpire * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: 'lax',
     };
 
     const refreshTokenOptions: ITokenOptions = {
-        expires: new Date(Date.now() + refreshTokenExpire * 1000),
-        maxAge: refreshTokenExpire * 1000,
+        expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
+        maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: 'lax',
 
