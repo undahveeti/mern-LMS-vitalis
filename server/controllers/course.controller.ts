b12d2d93e421e24465dcb7618618cf17dd0c9438
Andy Tran <undahvt@gmail.com>
2024-11-26 23:19:21 -0800

    question feature for courses function implemented

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -5,6 +5,7 @@ import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import { createCourse } from "../services/course.service";
 import CourseModel from "../models/course.model";
 import { redis } from "../utils/redis";
+import mongoose from "mongoose";
 
 // upload course
 export const uploadCourse = CatchAsyncError(async (req: Request, res: Response, next:NextFunction) => {
@@ -155,3 +156,46 @@ export const getCourseByUser = CatchAsyncError(async (req: Request, res: Respons
         return next(new ErrorHandler(error.message, 500));
     }
 })
+
+// add question in course
+interface IAddQuestionData{
+    question: string;
+    courseId: string;
+    contentId: string;
+}
+
+export const addQuestion = CatchAsyncError(async(req: Request, res: Response, next: NextFunction) => {
+    try {
+
+        const {question, courseId, contentId}: IAddQuestionData = req.body;
+        const course = await CourseModel.findById(courseId);
+
+        // checking if contentId is valid
+        if(!mongoose.Types.ObjectId.isValid(contentId)){
+            return next(new ErrorHandler("Invalid content id", 400));
+
+        }
+
+        const courseContent = course?.courseData.find((item:any) => item._id.equals(contentId));
+
+        if(!courseContent){
+            return next(new ErrorHandler("Invalid content id", 400));
+        }
+
+        // create new question object
+        const newQuestion:any = {
+            user:req.user,
+            question,
+            questionReplies:[],
+        };
+
+        // add this question to our course content
+
+        courseContent.questions.push(newQuestion);
+
+
+
+    }catch (error:any) {
+        return next(new ErrorHandler(error.message, 500));
+    }
+}) 
