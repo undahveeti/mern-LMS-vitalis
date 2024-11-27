53c56660fa372298f3d3d6fc1f075a91b46c5381
Andy Tran <undahvt@gmail.com>
2024-11-26 23:21:47 -0800

    routing for questions

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller";
+import { addQuestion, editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 
 const courseRouter = express.Router();
@@ -13,5 +13,7 @@ courseRouter.get("/get-course/:id", getSingleCourse);
 courseRouter.get("/get-courses", getAllCourses);
 
 courseRouter.get("/get-course-content/:id", isAuthenticated, getCourseByUser);
+
+courseRouter.put("/add-question", isAuthenticated, addQuestion);
 
 export default courseRouter;
