91997a53f4a5e608ecd918e367adc3bdd2e8e1ba
Andy Tran <undahvt@gmail.com>
2024-11-28 00:57:18 -0800

    update banner implemented

--- server/controllers/layout.controller.ts
+++ server/controllers/layout.controller.ts
@@ -23,6 +23,7 @@ export const createLayout = CatchAsyncError(async(req:Request,res:Response, next
                 folder: "layout",
             });
             const banner = {
+                type: "Banner",
                 image: {
                     public_id: myCloud.public_id,
                     url: myCloud.secure_url,
@@ -66,6 +67,71 @@ export const createLayout = CatchAsyncError(async(req:Request,res:Response, next
 
 
     }catch (error:any) {
+        return next(new ErrorHandler(error.message, 400));
+    }
+});
+
+// edit layout
+export const editLayout = CatchAsyncError(async(req:Request,res:Response, next:NextFunction)=>{
+    try {
+
+        const {type} = req.body;
+
+        if(type === "Banner"){
+            const bannerData:any = await LayoutModel.findOne({type: "Banner"});
+            const {image, title, subTitle} = req.body;
+
+            if(bannerData){
+                await cloudinary.v2.uploader.destroy(bannerData.image.public_id);
+            }
+
+            const myCloud = await cloudinary.v2.uploader.upload(image,{
+                folder: "layout",
+            });
+            const banner = {
+                type: "Banner",
+                image: {
+                    public_id: myCloud.public_id,
+                    url: myCloud.secure_url,
+                },
+                title,
+                subTitle
+            }
+
+            await LayoutModel.findByIdAndUpdate(bannerData.id,{banner});
+            
+        }
+        if(type === 'FAQ'){
+            const {faq} = req.body;
+            const faqItems = await Promise.all(
+                faq.map(async(item:any) => {
+                    return {
+                        question: item.question,
+                        answer: item.answer,
+                    };
+                })
+            )
+            await LayoutModel.create({type:"FAQ", faq:faqItems});
+        }
+        if(type === 'Categories'){
+            const {categories} = req.body;
+            const categoriesItems = await Promise.all(
+                categories.map(async(item:any) => {
+                    return {
+                        title: item.title,
+                    };
+                })
+            )
+            await LayoutModel.create({type:"Categories", categories: categoriesItems});
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Layout created successfully"
+        });
+
+
+    } catch (error: any){
         return next(new ErrorHandler(error.message, 400));
     }
 });
