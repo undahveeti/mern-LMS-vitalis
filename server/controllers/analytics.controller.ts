b600376ba3555f421d7c7b71adc25d2eb354c7cb
Andy Tran <undahvt@gmail.com>
2024-11-28 00:09:08 -0800

    implementation of order and course analytics w routing

--- server/controllers/analytics.controller.ts
+++ server/controllers/analytics.controller.ts
@@ -3,6 +3,8 @@ import ErrorHandler from "../utils/ErrorHandler";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import { generateLast12MonthsData } from "../utils/analytics.generator";
 import userModel from "../models/user.model";
+import CourseModel from "../models/course.model";
+import OrderModel from "../models/orderModel";
 
 // get users analytics -- only for admin
 
@@ -13,6 +15,39 @@ export const getUserAnalytics = CatchAsyncError(async (req: Request, res:Respons
         res.status(200).json({
             success: true,
             users,
+        });
+
+    } catch (error:any) {
+        return next(new ErrorHandler(error.message, 400));
+    }
+});
+
+// get courses analytics -- only for admin
+
+
+export const getCoursesAnalytics = CatchAsyncError(async (req: Request, res:Response, next:NextFunction) => {
+    try {
+        const courses = await generateLast12MonthsData(CourseModel);
+
+        res.status(200).json({
+            success: true,
+            courses,
+        });
+
+    } catch (error:any) {
+        return next(new ErrorHandler(error.message, 400));
+    }
+});
+
+// get order analytics -- only for admin
+
+export const getOrderAnalytics = CatchAsyncError(async (req: Request, res:Response, next:NextFunction) => {
+    try {
+        const orders = await generateLast12MonthsData(OrderModel);
+
+        res.status(200).json({
+            success: true,
+            orders,
         });
 
     } catch (error:any) {
--- server/routes/analytics.route.ts
+++ server/routes/analytics.route.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
-import { getUserAnalytics } from "../controllers/analytics.controller";
+import { getCoursesAnalytics, getOrderAnalytics, getUserAnalytics } from "../controllers/analytics.controller";
 const analyticsRouter = express.Router();
 
 analyticsRouter.get("/get-users-analytics", isAuthenticated, authorizeRoles("admin"), getUserAnalytics);
+
+analyticsRouter.get("/get-orders-analytics", isAuthenticated, authorizeRoles("admin"), getOrderAnalytics);
+
+analyticsRouter.get("/get-courses-analytics", isAuthenticated, authorizeRoles("admin"), getCoursesAnalytics);
 
 export default analyticsRouter;
