9e10fceddd737a5ee2a6c6d76085748378915a38
Andy Tran <undahvt@gmail.com>
2024-11-28 00:32:17 -0800

    create layout implementation

---
+++ server/controllers/layout.controller.ts
@@ -0,0 +1,50 @@
+import { Request, NextFunction, Response } from "express";
+
+import ErrorHandler from "../utils/ErrorHandler";
+import { CatchAsyncError } from "../middleware/catchAsyncErrors";
+import LayoutModel from "../models/layout.model";
+import cloudinary from "cloudinary";
+
+// create layout
+export const createLayout = CatchAsyncError(async(req:Request,res:Response, next:NextFunction)=>{
+    try {
+        const {type} = req.body;
+        if(type === "Banner"){
+            const {image, title, subTitle} = req.body;
+
+            const myCloud = await cloudinary.v2.uploader.upload(image,{
+                folder: "layout",
+            });
+            const banner = {
+                image: {
+                    public_id: myCloud.public_id,
+                    url: myCloud.secure_url,
+                },
+                title,
+                subTitle
+            }
+
+            await LayoutModel.create(banner);
+            
+        }
+        if(type === 'FAQ'){
+            const {faq} = req.body;
+            
+            await LayoutModel.create(faq);
+        }
+        if(type === "Categories"){
+            const {categories} = req.body;
+
+            await LayoutModel.create(categories);
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Layout created successfully",
+        });
+
+        
+    }catch (error:any) {
+        return next(new ErrorHandler(error.message, 400));
+    }
+});
