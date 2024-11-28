5bcc55ef6076e0fdcf5e468f7268f05c22bf992a
Andy Tran <undahvt@gmail.com>
2024-11-28 14:10:06 -0800

    heading page creation and utility

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,13 +1,18 @@
 
 'use client'
 import React, {FC, useState} from "react";
+import Heading from "./utils/Heading";
 
 interface Props {}
 
 const Page: FC<Props> = (props) => {
     return (
       <div>
-        
+        <Heading
+          title="Vitalis Solutions Group"
+          description="Transformative solutions from industry experts ready to address regulatory and workforce pressures"
+          keywords="Data Analysis, Data Analytics, Linear Regression Models"
+        />
       </div>
     )
 };
