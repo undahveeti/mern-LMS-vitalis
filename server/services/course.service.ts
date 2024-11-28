6a725c2ff9438fe0b9a3cd183ad620a7b4dabed9
Andy Tran <undahvt@gmail.com>
2024-11-27 21:49:29 -0800

    get all courses implemented w model and service

--- server/services/course.service.ts
+++ server/services/course.service.ts
@@ -2,7 +2,7 @@ import {Response} from "express";
 import CourseModel from "../models/course.model";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 
-// create course
+// create course is for business plans, 
 export const createCourse = CatchAsyncError(async (data: any, res: Response) => {
     const course = await CourseModel.create(data);
     res.status(201).json({
@@ -10,3 +10,14 @@ export const createCourse = CatchAsyncError(async (data: any, res: Response) => 
         course
     });
 });
+
+// get all courses
+
+export const getAllCoursesService = async (res: Response) => {
+    const courses = await CourseModel.find().sort({createdAt:-1});
+
+    res.status(201).json({
+        success: true,
+        courses
+    })
+}
