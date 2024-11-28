4759b649aa436c0a6682d6d10c7443c08613c7db
Andy Tran <undahvt@gmail.com>
2024-11-27 20:19:01 -0800

    reading notification implemented for admin

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -19,3 +19,31 @@ export const getNotifications = CatchAsyncError(async (req: Request, res: Respon
         return next(new ErrorHandler(error.message, 500));
     }
 });
+
+// update notification status -- only for admin
+
+export const updateNotification =  CatchAsyncError(async (req: Request, res: Response, next: NextFunction)=> {
+    try {
+        const notification = await NotificationModel.findById(req.params.id);
+
+        if(!notification){
+            return next(new ErrorHandler("Notification not found", 500)); 
+        } else {
+            notification.status ? notification.status = 'read': notification?.status; 
+        }
+        
+        await notification.save();
+
+        // we will update on the frontend
+        const notifications = await NotificationModel.find().sort({
+            createdAt: -1,
+        });
+
+        res.status(201).json({
+            success: true,
+            notifications,
+        })
+    } catch (error:any){
+        return next(new ErrorHandler(error.message, 500));
+    }
+})
