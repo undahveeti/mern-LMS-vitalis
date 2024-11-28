4c0e87ef7e34d9ef8ea1509e29bb8947fdf9916d
Andy Tran <undahvt@gmail.com>
2024-11-28 01:00:19 -0800

    routing for edit layout

--- server/routes/layout.route.ts
+++ server/routes/layout.route.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
-import { createLayout } from "../controllers/layout.controller";
+import { createLayout, editLayout } from "../controllers/layout.controller";
 
 const layoutRouter = express.Router();
 
 layoutRouter.post("/create-layout",isAuthenticated, authorizeRoles("admin"), createLayout);
+
+layoutRouter.post("/edit-layout",isAuthenticated, authorizeRoles("admin"), editLayout);
 
 export default layoutRouter;
