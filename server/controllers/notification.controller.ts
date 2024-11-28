18b515c292b41e690227ce323eb70f57f2cca52a
Andy Tran <undahvt@gmail.com>
2024-11-27 21:40:00 -0800

    node cron test

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -2,7 +2,7 @@ import NotificationModel from "../models/notificationModel";
 import { NextFunction, Response, Request } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import ErrorHandler from "../utils/ErrorHandler";
-
+import cron from "node-cron";
 
 // get all notifs -- only for admin
 export const getNotifications = CatchAsyncError(async (req: Request, res: Response, next: NextFunction)=> {
@@ -46,4 +46,11 @@ export const updateNotification =  CatchAsyncError(async (req: Request, res: Res
     } catch (error:any){
         return next(new ErrorHandler(error.message, 500));
     }
+})
+
+// delte notification -- only admin
+// after 5 seconds
+cron.schedule("*/5 * * * * *", function(){
+    console.log("--------------");
+    console.log("running cron");
 })
