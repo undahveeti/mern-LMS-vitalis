dfe525e508638204b1c8497de6584539ead44c94
Andy Tran <undahvt@gmail.com>
2024-11-30 00:26:02 -0800

    ui friendly features / sign up w google

--- client/app/components/Auth/Login.tsx
+++ client/app/components/Auth/Login.tsx
@@ -20,7 +20,7 @@ const schema = Yup.object().shape({
   password: Yup.string().required("Please enter your password!").min(8),
 });
 
-const Login: FC<Props> = (props) => {
+const Login: FC<Props> = ({ setRoute }) => {
   const [show, setShow] = useState(false);
 
   const formik = useFormik({
@@ -34,7 +34,7 @@ const Login: FC<Props> = (props) => {
   const { errors, touched, values, handleChange, handleSubmit } = formik;
   return (
     <div className="w-full">
-      <h1 className={`${styles.title}`}>Login with Vitalis Solutions Group</h1>
+      <h1 className={`${styles.title}`}>Login with Vitalis</h1>
       <form onSubmit={handleSubmit}>
         <label className={`${styles.label}`} htmlFor="email">
           Enter your Email
@@ -58,31 +58,54 @@ const Login: FC<Props> = (props) => {
             Enter your password
           </label>
           <input
-            type = {!show ? "password" : "text"}
-            name = "password"
-            value = {values.password}
-            onChange = {handleChange}
-            id = "password"
-            placeholder = "password!@%"
+            type={!show ? "password" : "text"}
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+            id="password"
+            placeholder="password!@%"
             className={`${
-                errors.password && touched.password && "border-red-500"
-              } ${styles.input}`}
+              errors.password && touched.password && "border-red-500"
+            } ${styles.input}`}
+          />
+          {!show ? (
+            <AiOutlineEyeInvisible
+              className="absolute bottom-3 right-2 z-1 cursor-pointer"
+              size={20}
+              onClick={() => setShow(true)}
             />
-            {!show ? (
-              <AiOutlineEyeInvisible
-                className="absolute bottom-3 right-2 z-1 cursor-pointer"
-                size={20}
-                onClick={() => setShow(true)}
-              />
-            ) : (
-              <AiOutlineEye
-                className="absolute bottom-3 right-2 z-1 cursor-pointer"
-                size={20}
-                onClick={() => setShow(false)}
-              />
-            )}
+          ) : (
+            <AiOutlineEye
+              className="absolute bottom-3 right-2 z-1 cursor-pointer"
+              size={20}
+              onClick={() => setShow(false)}
+            />
+          )}
+          {errors.password && touched.password && (
+            <span className="text-red-500 pt-2 block">{errors.password}</span>
+          )}
         </div>
+        <div className="w-full mt-5">
+          <input type="submit" value="Login" className={`${styles.button}`} />
+        </div>
+        <br />
+        <h5 className="text-center pt-4 font-Poppins text-[14px] text-black dark:text-white">
+          Or join with
+        </h5>
+        <div className="flex items-center justify-center my-3">
+          <FcGoogle size={30} className="cursor-pointer mr-2" />
+        </div>
+        <h5 className="text-center pt-4 font-Poppins text-[14px]">
+          Don't have any account?{" "}
+          <span
+            className="text-[#2190ff] pl-1 cursor-pointer"
+            onClick={() => setRoute("Sign-Up")}
+          >
+            Sign up
+          </span>
+        </h5>
       </form>
+      <br/>
     </div>
   );
 };
