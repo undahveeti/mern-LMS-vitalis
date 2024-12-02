bfef572e955952714fbf32069031551d0308c238
Andy Tran <undahvt@gmail.com>
2024-12-02 14:38:59 -0800

    ignoring eslint checks during build

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,8 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains:['res.cloudinary.com'],
-  }
+    domains: ['res.cloudinary.com'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint checks during the build process
+  },
 };
 
 export default nextConfig;
+
