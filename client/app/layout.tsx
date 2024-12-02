7f3fbb01d9ce8d6eadab078a5cdcff0bfda71fd0
Andy Tran <undahvt@gmail.com>
2024-12-01 17:50:59 -0800

    icon is replaced with user automatically

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -7,6 +7,9 @@ import { Toaster } from "react-hot-toast";
 import { Providers } from "./Provider";
 
 import {SessionProvider} from 'next-auth/react';
+import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
+
+import Loader from "./components/Loader/Loader";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -28,14 +31,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body
         className={`${poppins.variable} ${josefin.variable} !bg-white bg-no-repeat dark:bg-gradient-to-b dark:from-gray-900 dark:to-customBlue duration-300`}
       >
         <Providers>
           <SessionProvider>
             <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-              {children}
+              <Custom>
+                {children}
+              </Custom>
               <Toaster position="top-center" reverseOrder={false} />
             </ThemeProvider>
           </SessionProvider>
@@ -44,3 +49,14 @@ export default function RootLayout({
     </html>
   );
 }
+
+const Custom: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { isLoading } = useLoadUserQuery({});
+
+  return (
+    <>
+    {
+      isLoading ? <Loader /> : <>{children} </> }
+    </>
+  )
+};
