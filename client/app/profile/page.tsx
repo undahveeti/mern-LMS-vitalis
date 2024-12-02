149973bc7b29d0a1da9a74b67c718a6a3c4e5ca2
Andy Tran <undahvt@gmail.com>
2024-12-01 17:59:37 -0800

    hooks to help create page for profile

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+'use client'
+import React, { FC, useState } from "react";
+import Protected from "../hooks/useProtected";
+import Heading from "../utils/Heading";
+import Header from "../components/Header";
+import { useSelector } from "react-redux";
 
-type Props = {}
+type Props = {};
 
-const page = (props: Props) => {
+const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(5);
+  const [route, setRoute] = useState("Login");
+
   return (
-    <div>page</div>
-  )
-}
+    <div className="min-h-screen">
+      <Protected>
+      <Heading
+          title="Vitalis Solutions Group"
+          description="Transformative solutions from industry experts ready to address regulatory and workforce pressures"
+          keywords="Data Analysis, Data Analytics, Linear Regression Models"
+        />
+        <Header
+          open={open}
+          setOpen={setOpen}
+          activeItem={activeItem}
+          setRoute={setRoute}
+          route={route}
+        />
+      </Protected>
+    </div>
+  );
+};
 
-export default page
+export default Page;
