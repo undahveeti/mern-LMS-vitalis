34c1be331415cf0c7507f0d7213941ba4aed1c9a
Andy Tran <undahvt@gmail.com>
2024-12-01 17:52:29 -0800

    loading animation and ui responsiveness

--- client/app/components/Loader/Loader.tsx
+++ client/app/components/Loader/Loader.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import "./Loader.css";
 
-type Props = {}
-
-const Loader = (props: Props) => {
+const Loader = () => {
   return (
-    <div>Loader</div>
-  )
-}
+    <div className="flex justify-center items-center h-screen">
+      <div className="loader"></div>
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
